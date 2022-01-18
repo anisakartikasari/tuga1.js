cf3c0daa4b5cb29d8a2913021e153a750ffb9d9b
anisakartikasari <03anisaks@gmail.com>
2022-01-17 22:24:59 -0800

    tugas

---
+++ soalno1.js
@@ -0,0 +1,15 @@
+function perkalian(years){
+    console.log(years*365)
+}
+
+perkalian(17);
+perkalian(69);
+perkalian(79);
+
+var perkalian3 = 123;
+
+if (Number.isInteger(perkalian3)) {
+    console.log("asd is integer");
+} else {
+    console.log("asd is not integer");
+}
---
+++ soalno3.js
@@ -0,0 +1,10 @@
+function element(number){
+    if(number==0){
+        console.log("even")
+    } else
+    console.log("odd")
+}
+
+element (0);
+element (1);
+element (5)
